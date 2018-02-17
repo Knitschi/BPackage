6f1c9582309dbe8176df3a8fcd1fd216f81bfef6
Knitschi <knitschi@freenet.de>
2018-02-17 13:42:26 +0100

    Add basic files

---
+++ b.cpp
@@ -0,0 +1,10 @@
+#include <lib0/lib0.h>
+
+namespace lb0
+{
+    void lib0Function()
+    {
+        
+    }
+}
+
---
+++ b.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <lib0/lb0_export.h>
+
+namespace lb0
+{
+    LB0_EXPORT void lib0Function();
+}
