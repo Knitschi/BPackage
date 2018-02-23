78cc68fbb9717a2a5d99246b00b4a4e535ab02d4
Knitschi <knitschi@freenet.de>
2018-02-23 16:38:27 +0100

    rename file

---
+++ function.cpp
@@ -0,0 +1,11 @@
+#include <BPackage/function.h>
+
+namespace b
+{
+    void function()
+    {
+        
+    }
+}
+
+
---
+++ function.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <BPackage/b_export.h>
+
+namespace b
+{
+    B_EXPORT void function();
+}
+
